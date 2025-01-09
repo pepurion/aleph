2e9b9f962fcb0a4c97daf4fa97406c968e7933f0
DPepperoni <129223708+DPepperoni@users.noreply.github.com>
2025-01-09 06:12:27 +0200

    Quartz sync: Jan 9, 2025, 6:12 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -19,7 +19,7 @@ const config: QuartzConfig = {
     baseUrl: "pepurion.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
-    generateSocialImages: false,
+    generateSocialImages: true,
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
